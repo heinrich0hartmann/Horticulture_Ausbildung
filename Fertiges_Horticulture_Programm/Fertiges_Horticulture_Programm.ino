365c8d155c4fc2a7fc405ff304cc4ff25cd70513
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-11-09 09:04:49 +0100

    Lichtsensor Libary

--- Fertiges_Horticulture_Programm/Fertiges_Horticulture_Programm.ino
+++ Fertiges_Horticulture_Programm/Fertiges_Horticulture_Programm.ino
@@ -1,10 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <AS_BH1750.h>
 
+AS_BH1750 sensor;
+
+void setup()
+{
+    Serial.begin(9600);
+
+    // for normal sensor resolution (1 lx resolution, 0-65535 lx, 120ms, no PowerDown)
+    // use: sensor.begin(RESOLUTION_NORMAL, false);
+
+    if(!sensor.begin()) {
+        Serial.println("BH1750 init failed!");
+        while(true);
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+    float lux = sensor.readLightLevel();
+    Serial.println("Light level: " + String(lux) + " lx");
+    delay(1000);
 }
-Dieses ist ein Bearbeitungstest
