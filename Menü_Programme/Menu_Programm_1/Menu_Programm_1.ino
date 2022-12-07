131b342371fe1089786526ed303f4ec561e82940
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-12-07 14:29:14 +0100

    Variablen Für Einstellparameter Relais

--- Menü_Programme/Menu_Programm_1/Menu_Programm_1.ino
+++ Menü_Programme/Menu_Programm_1/Menu_Programm_1.ino
@@ -109,8 +109,67 @@ void setup()
 //Funktion für Wert Ausgabe auf Overview Display
 int OverviewAusgabe ()  //() müssen vielleicht noch Übergabe Parameter
 {
+  lcd.setCursor(0,0);   //Temperatur
+  lcd.print("TMP: " + String(sensors.getTempCByIndex(0)));  //Temperatur ausgeben
   
-}
+  lcd.setCursor(0,1);   //Lichtintensität
+  lcd.print("LGT: ");
+  lcd.setCursor(5,1);
+  lcd.print(lux);       //Variable lux ausgeben
+
+  lcd.setCursor(0,2);   //Humitity
+  lcd.print("HUM: ");
+  lcd.setCursor(5,2);
+  lcd.print(prozent);   //Humidity in Prozent ausgeben
+
+
+  lcd.setCursor(0,3);   //Wasserstand
+  lcd.print("H2O: ");
+  lcd.setCursor(5,3);
+  lcd.print(distance);  //Variable distance ausgeben
+
+  //Für die Relais
+  if (prozent <= ONrWasser){                                     //Grenze Wasser ON Variable: ONrWasser
+  digitalWrite(rWasser, HIGH);   //Wasser ein
+  
+  lcd.setCursor(13,2);
+  lcd.print("R1: ON");
+
+  lcd.setCursor(13,3);
+  lcd.print("> Back");
+  }
+  if (prozent >= OFFrWasser){                                    //Grenze Wasser OFF Variable: OFFrWasser
+  digitalWrite(rWasser, LOW);  //Wasser aus
+
+  lcd.setCursor(13,2);
+  lcd.print("R1: OFF");
+  }
+
+  //Licht
+  if (lux <= ONrLicht){                                          //Grenze Licht ON Variable: ONrLicht
+  digitalWrite(rLicht, HIGH);   //Lampe ein
+  lcd.setCursor(13,1);
+  lcd.print("R2: ON");
+  }
+  else{
+  digitalWrite(rLicht, LOW);  //Licht aus
+  lcd.setCursor(13,1);
+  lcd.print("R2: OFF"); 
+  }
+
+  //Belüftung
+  if (temp >= ONrLuft){                                          //Grenze Lüfter ON Variable: ONrLuft
+  digitalWrite(rLuft, HIGH);   //Lüfter ein 
+  lcd.setCursor(13,0);
+  lcd.print("R3: ON");
+  }
+  if (temp <= OFFrLuft){                                         //Grenze Lüfter OFF Variable: OFFrLuft
+  digitalWrite(rLuft, LOW); //Lüfter aus
+  lcd.setCursor(13,0);
+  lcd.print("R3: OFF");
+  }
+
+} //Schlussklammer Overview Subroutine
 
 
 //Funktion für die LCD Ausgabe
